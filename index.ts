1b162cbc141f0477c7540d7876a382ea1c1ba3f8
ericcnguyen04 <ericc.win@gmail.com>
2023-03-13 18:22:12 -0400

    created "class" or interface

--- index.ts
+++ index.ts
@@ -1,9 +1,32 @@
 import * as _ from 'lodash';
 
-async function hello() {
-    return 'world'
+// async function hello() {
+//     return 'world'
+// }
+
+// let lucky: number;
+
+// lucky = '23'
+
+type Style = 'bold' | 'italic';
+
+let font: Style;
+
+font = 'something'
+
+interface Person {
+    first: string;
+    last: string;
+    [key: string]: any
 }
 
-let lucky: any = 23;
+const person = {
+    first: "Jeff",
+    last: 'Delaney'
+}
 
-lucky = '23'
+const person2 = {
+    first: "Jeff",
+    last: 'Bezos'
+    rich: true
+}
